81c327398a57a0a4b2c4e224cb35cf67267d6f7c
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-06-04 15:59:12 +0600

    feat: authentication middlware added in django project

--- core/settings.py
+++ core/settings.py
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'core.middleware.AdminOnlyMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
