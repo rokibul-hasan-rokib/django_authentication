346b16b7f57bf4621a5d4a7469b9ad4c2217af5e
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-06-04 09:53:53 +0600

    url added

--- core/urls.py
+++ core/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+     path('admin/', admin.site.urls),
+    path('', include('accounts.urls')),          # Custom app
+    path('accounts/', include('django.contrib.auth.urls')),  # Login/Logout views
 ]
 
 
